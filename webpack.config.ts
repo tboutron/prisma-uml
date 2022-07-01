849c92f64fbab6db41f785ad3073e6a9eb876b05
Théo Boutron <tboutron@stot.fr>
2022-07-02 00:00:45 +0200

    npm: fix types and version

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,12 @@
-import webpack from 'webpack';
-import path from 'path';
-import nodeExternals from 'webpack-node-externals';
-import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import webpack from 'webpack'
+import path from 'path'
+import nodeExternals from 'webpack-node-externals'
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin'
 
-const nodeEnv = process.env.NODE_ENV || 'development';
-const isProd = nodeEnv === 'production';
+//const nodeEnv = process.env.NODE_ENV || 'development';
+//const isProd = nodeEnv === 'production';
 
-delete process.env.TS_NODE_PROJECT;
+delete process.env.TS_NODE_PROJECT
 const webpackconfiguration: webpack.Configuration = {
   entry: path.resolve(__dirname, 'src', 'index.ts'),
   target: 'node',
@@ -25,6 +25,6 @@ const webpackconfiguration: webpack.Configuration = {
     rules: [{ test: /\.(ts|js)x?$/, use: ['babel-loader', 'source-map-loader'], exclude: /node_modules/ }],
   },
   plugins: [new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true })],
-};
+}
 
-export default webpackconfiguration;
+export default webpackconfiguration
